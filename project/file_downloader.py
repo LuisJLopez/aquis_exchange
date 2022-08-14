d6f08c4bb290e4e13e33e438fc1dcc7b6d3ac295
Luis Lopez <Luisjlop@gmail.com>
2022-08-14 19:13:02 +0100

    fix: removing unnecessary comments

--- project/file_downloader.py
+++ project/file_downloader.py
@@ -13,14 +13,13 @@ class FileDownloader:
     def __init__(self, url: str, outbound_directory: str) -> None:
         self.url: str = url
         self.dataset_file_dir: str = outbound_directory
-        self.binary_bytes: str = ONE_MB_IN_BINARY_BYTES
+        self.chunk_size: str = ONE_MB_IN_BINARY_BYTES
 
     def download(self) -> None:
         headers = {}
 
-        # append modified header only if we already have file locally stored
         if os.path.exists(self.dataset_file_dir):
-            modified_time = os.path.getmtime(self.dataset_file_dir)
+            modified_time: float = os.path.getmtime(self.dataset_file_dir)
             headers["if-modified-since"] = formatdate(modified_time, usegmt=True)
 
         response = requests.get(self.url, headers=headers, stream=True)
@@ -33,10 +32,9 @@ class FileDownloader:
         if response.status_code == requests.codes.ok:
             # write file one line at a time, don't load all of it into memory
             with open(self.dataset_file_dir, "wb") as file:
-                for chunk in response.iter_content(chunk_size=self.binary_bytes):
+                for chunk in response.iter_content(chunk_size=self.chunk_size):
                     file.write(chunk)
 
-            # fetch and store the new modified header for the next download
             if last_modified := response.headers.get("last-modified"):
                 new_modified_time = parsedate_to_datetime(last_modified).timestamp()
                 os.utime(
